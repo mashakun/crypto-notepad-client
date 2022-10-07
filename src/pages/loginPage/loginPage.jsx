280206a11ecfcfbc10e82b138a98223304b26acc
mashakun <shakunica@gmail.com>
2022-10-08 01:02:55 +0300

    login functionality

--- src/pages/loginPage/loginPage.jsx
+++ src/pages/loginPage/loginPage.jsx
@@ -3,14 +3,72 @@ import { useState } from "react";
 import { useForm } from 'react-hook-form';
 import { useNavigate, Navigate } from "react-router-dom";
 
+import axios from '../../axios.js';
+
+
 const LoginPage = (props) => {
 
     const [login, setLogin] = useState(true);
     const [auth, setAuth] = useState(false);
 
-    // fetch auth
-    // fetch register
-    // они оба генерят jwt и сохраняют в localStorage
+    const { register: registerLogin, handleSubmit: handleLogin, } = useForm({
+        defaultValues: {
+            password: 'password',
+            login: 'login'
+        },
+        mode: 'onChange',
+    });
+
+    const { register: registerRegister, handleSubmit: handleRegister, } = useForm({
+        defaultValues: {
+            email: 'mail@mail.mail',
+            password: 'password',
+            name: 'name',
+            login: 'login'
+        },
+        mode: 'onChange',
+    });
+
+    const onLogin = async (values) => {
+
+        console.log(values);
+
+        // const data = await axios.post('/login', values);
+        // console.log('login data: ', data);
+
+        // if (!data.payload) {
+        //     return alert('No authorization');
+        // }
+
+        // if ('token' in data.payload) {
+        //     window.localStorage.setItem('token', data.payload.token);
+        // } else {
+        //     alert('No authorization');
+        // }
+
+        setAuth(true);
+    }
+
+    const onRegister = async (values) => {
+
+        console.log(values);
+
+        // const data = await axios.post('/register', values);
+        // console.log('register data: ', data);
+
+        // if (!data.payload) {
+        //     return alert('No authorization');
+        // }
+
+        // if ('token' in data.payload) {
+        //     window.localStorage.setItem('token', data.payload.token);
+        // } else {
+        //     alert('No authorization');
+        // }
+
+        setAuth(true);
+    }
+
     // в это время axios чекает наличие токена и добавляет в хедер после аутентификации(??????? сработает ли)
 
     const navigate = useNavigate();
@@ -26,20 +84,22 @@ const LoginPage = (props) => {
 
             {
                 login ?
-                    <form>
-                        <input type="text" required></input>
-                        <input type="text" required></input>
+                    <form onSubmit={handleLogin(onLogin)}>
+                        <input type="text" {...registerLogin("login")} required></input>
+                        <input type="text" {...registerLogin("password")} required></input>
 
-                        <button type="submit" onClick={() => setAuth(true)}>sign in</button>
+                        <button type="submit">sign in</button>
                     </form>
-                    :
-                    <form>
-                        <input type="text" required></input>
-                        <input type="text" required></input>
-                        <input type="email" required></input>
-                        <input type="text" required></input>
 
-                        <button type="submit" onClick={() => setAuth(true)}>sign in</button>
+                    :
+
+                    <form onSubmit={handleRegister(onRegister)}>
+                        <input type="text" required {...registerRegister("login")}></input>
+                        <input type="text" required {...registerRegister("password")}></input>
+                        <input type="email" required {...registerRegister("email")}></input>
+                        <input type="text" required {...registerRegister("name")}></input>
+
+                        <button type="submit">sign in</button>
                     </form>
             }
         </div>
--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -1,13 +1,49 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, Navigate } from "react-router-dom";
+
+import axios from "../../axios.js";
 
 const MainPage = (props) => {
 
-    // явно нужен стейт со списком файлов, обновляется после create и delete
-    // нужен стейт с curr файлом
-    // нужен стейт с текстом curr файла
+    const [isText, setIsText] = useState(false);
+    const [files, setFiles] = useState([]);
+    const [currFile, setCurrFile] = useState({fileName: null, content: null});
 
 
+    const onCreate = async () => {
+        const newFile = {
+            fileName: "New_file",
+            content: "",
+        }
+
+        const data = await axios.post('/', newFile);
+        console.log("Creation: ", data);
+
+        setCurrFile({fileName: data.fileName, id: data.id, content: data.content});
+        setFiles([...files, {fileName: data.fileName, id: data.id}]);
+        setIsText(true);
+    }
+
+    const onDelete = async () => {
+        const data = await axios.delete(`/${currFile.id}`);
+        console.log("Deleted: ", data);
+
+        let id = files.findIndex((el) => el.id === currFile.id);
+        setFiles(files.splice(id, 1));
+        setCurrFile({fileName: null, id: null, content: null});
+        setIsText(false);
+    }
+
+    const onSave = async (text) => {
+        const data = await axios.patch(`/${currFile.id}`, {content: text});
+        console.log("Saved: ", data);
+
+        setCurrFile({fileName: currFile.fileName, content: text});
+        setIsText(true);
+    }
+
+    const onBack = async () => {}
 
     return (
         <div>
@@ -16,7 +52,7 @@ const MainPage = (props) => {
             <button>save</button>
             <button>back</button>
 
-            <textarea>jbaslfkjskdskfn;dsjfsdkfgsdgkns</textarea>
+            <textarea defaultValue={"skdjflakf"}></textarea>
 
         </div>
     );
