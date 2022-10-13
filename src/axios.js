b03e629570bf7080c09615389b592534ccfad508
mashakun <shakunica@gmail.com>
2022-10-13 16:50:39 +0300

    meh

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'http://notepad.nikuchin.fun',
-    baseURL: 'http://localhost:3001',
+    baseURL: 'http://notepad.nikuchin.fun',
+    // baseURL: 'http://localhost:3001',
 });
 
 
